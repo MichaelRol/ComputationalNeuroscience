dfe2a73036c310e093ffa1de990fdac2dd1c3f7b
MichaelRol <michael.rollins@hotmail.co.uk>
2019-03-05 19:11:57 +0000

    Updated file for the submission - didn't realise they'd changed so much

--- CW1/seven_segments.py
+++ CW1/seven_segments.py
@@ -1,7 +1,11 @@
+#for the submission uncomment the submission statements
+#see submission.README
+
 import math
 
+#from submission import *
 
-w = [[0 for x in range(11)] for y in range(11)] 
+weight_matrix = [[0 for x in range(11)] for y in range(11)] 
 N = 3.0
 
 
@@ -13,17 +17,21 @@ one=[-1,-1,1,-1,-1,1,-1,1,-1,-1,-1]
 for i in range(0, 11):
     for j in range(0, 11):
         if i != j:
-            w[i][j] = 1/N * (six[i]*six[j] + three[i]*three[j] + one[i]*one[j])
+            weight_matrix[i][j] = 1/N * (six[i]*six[j] + three[i]*three[j] + one[i]*one[j])
 
-def E(pattern):
-    e = 0
+
+def hopfield(x):
+    y = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     for i in range(0, 11):
+        val = 0
         for j in range(0, 11):
             if i != j:
-                e += pattern[i]*w[i][j]*pattern[j]
-    e *= -0.5
-    return e
-
+                val += weight_matrix[i][j]*x[j]
+        if val > 0:
+            y[i] = 1
+        if val < 0:
+            y[i] = -1
+    return y
 
 def seven_segment(pattern):
 
@@ -73,28 +81,23 @@ def seven_segment(pattern):
             number+=pow(2,i)
     print(int(number))
         
+#submission=Submission("michael_rollins")
+#submission.header("Michael Rollins")
+
+six=[1,1,-1,1,1,1,1,-1,1,1,-1]
+three=[1,-1,1,1,-1,1,1,1,1,-1,-1]
+one=[-1,-1,1,-1,-1,1,-1,1,-1,-1,-1]
+
 seven_segment(three)
-seven_segment(six)  
+seven_segment(six)
 seven_segment(one)
 
+##this assumes you have called your weight matrix "weight_matrix"
+#submission.section("Weight matrix")
+#submission.matrix_print("W",weight_matrix)
 
-            
-
-def hopfield(x):
-    y = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    for i in range(0, 11):
-        val = 0
-        for j in range(0, 11):
-            if i != j:
-                val += w[i][j]*x[j]
-        if val > 0:
-            y[i] = 1
-        if val < 0:
-            y[i] = -1
-    return y
-
-            
 print("test1")
+#submission.section("Test 1")
 
 test=[1,-1,1,1,-1,1,1,-1,-1,-1,-1]
 
@@ -105,13 +108,25 @@ while(1):
     else:
         test = next
         seven_segment(test)
-    
+
+#submission.seven_segment(test)
+##for COMSM0027
+
+##where energy is the energy of test
+#submission.qquad()
+#submission.print_number(energy)
+
+##this prints a space
+#submission.qquad()
 
 #here the network should run printing at each step
+#for the final submission it should also output to submission on each step
 
 print("test2")
+#submission.section("Test 2")
 
 test=[1,1,1,1,1,1,1,-1,-1,-1,-1]
+    
 
 while(1):
     next = hopfield(test)
@@ -121,4 +136,18 @@ while(1):
         test = next
         seven_segment(test)
 
+#submission.seven_segment(test)
+
+##for COMSM0027
+##where energy is the energy of test
+#submission.qquad()
+#submission.print_number(energy)
+
+##this prints a space
+#submission.qquad()
+
 #here the network should run printing at each step
+#for the final submission it should also output to submission on each step
+
+
+#submission.bottomer()
