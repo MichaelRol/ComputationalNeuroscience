87e4ed23f7c8bc218cd5bd582b34191fa931cbbd
MichaelRol <michael.rollins@hotmail.co.uk>
2019-03-23 15:52:13 +0000

    CoV for q2 finished

--- CW2/load.py
+++ CW2/load.py
@@ -1,3 +1,4 @@
+import numpy as np
 
 def load_data(filename,T):
 
@@ -5,6 +6,19 @@ def load_data(filename,T):
 
     return data_array
 
+def CoV(rho):
+    intervals = []
+    time = 0
+    for x in rho:
+        if x == 1:
+            intervals.append(time)
+            time = 0
+        time += 0.002
+    mean = np.mean(intervals)
+    stdev = np.std(intervals)
+    print(rho[0:100])
+    print(intervals[0:100])
+    return(stdev/mean)
 
 #spikes=[int(x) for x in load_data("rho.dat")]
 spikes=load_data("rho.dat",int)
@@ -17,3 +31,5 @@ stimulus=load_data("stim.dat",float)
 
 print(len(stimulus))
 print(stimulus[0:5])
+
+
